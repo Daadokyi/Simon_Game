81647c9c8e40301d94709dc7086705691db55fc0
Daadokyi <Kwekudokyi@outlook.com>
2023-11-24 05:47:07 +0000

    Pattern created

--- Game.js
+++ Game.js
@@ -1,3 +1,14 @@
+var gamePattern = [];
+// creating an array to store the colours of the various buttons
+    var buttonColours = ["red", "blue", "green", "yellow"];
 function nextSequencce(){
-Math.floor(Math.random()*4) +1
-}
+    var randomNumber =  Math.floor(Math.random()*4);
+        console.log("Random Number: " + randomNumber);
+// Return the generated random number (if needed)
+    return randomNumber;
+} 
+    // Create a new variable called randomChosenColour
+var randomChosenColour = buttonColours[randomNumber];
+
+
+
