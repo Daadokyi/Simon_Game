3ef622826088b442400381281b2573c069c4a5dc
Daadokyi <Kwekudokyi@outlook.com>
2023-12-02 23:02:01 +0000

    bug fixed

--- Game.js
+++ Game.js
@@ -1,11 +1,12 @@
 // adding jQuerry to select the colors randomly
 $(document).ready(function() {
+
     var buttonColours = ["red", "blue", "green", "yellow"];
     var gamePattern = [];
+
     function nextSequencce() {
         var randomNumber = Math.floor(Math.random() * 4);
-        console.log("Random Number: " + randomNumber);
-
+    
         // Create a new variable called randomChosenColour
         var randomChosenColour = buttonColours[randomNumber];
 
@@ -13,7 +14,7 @@ $(document).ready(function() {
         gamePattern.push(randomChosenColour);
 
         // Selecting the button randomly by ID
-        var selectedButton = $("btn" + randomChosenColour);
+        var selectedButton = $("#btn" + randomChosenColour);
 
         $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     }
